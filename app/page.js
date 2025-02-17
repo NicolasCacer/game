cdb4dc73263fc61b93890191b01c4ca3ae917a5c
Nicolas <njcaceres04@gmail.com>
2025-02-16 22:08:19 -0500

    Deploying to Vercel

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import io from "socket.io-client";
 import "dotenv/config";
 
+console.log("Connecting to:", process.env.NEXT_PUBLIC_SOCKET_IO);
 const socket = io(process.env.NEXT_PUBLIC_SOCKET_IO);
 
 export default function Home() {
